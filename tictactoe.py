65219f733dde372452c512fb00c28a2493baa667
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-15 16:00:19 -0400

    Create function to print board to console.

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+
+# prints board to display
+def print_board(board):
+    print("   |   |   ")
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("   |   |   ")
+    print("-----------")
+    print("   |   |   ")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("   |   |   ")
+    print("-----------")
+    print("   |   |   ")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+    print("   |   |   ")
+
+board = [" "," "," "," "," "," "," "," "," "]
